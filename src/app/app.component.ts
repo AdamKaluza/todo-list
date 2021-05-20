9b942e2476de16bdd5349c02c2bc47225a47e02c
Adam Kaluza <adam.kaluza.97@gmail.com>
2021-05-20 22:22:59 +0200

    added list based task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,17 +8,29 @@ import {Task} from "./task";
 })
 export class AppComponent {
   config: { [key: string]: string | Date } = null;
-  tasks: Task[] = [{
-
-
-
-  }]
+  tasks: Task[] = [
+    {
+      name: 'Siłownia',
+      deadline: '2020-01-02',
+      done: false,
+    },
+    {
+      name: 'Nauka Angulara',
+      deadline: '2020-01-03',
+      done: false,
+    },
+    {
+      name: 'Sprzątanie kuwety',
+      deadline: '2020-01-04',
+      done: false,
+    },
+  ];
 
   constructor() {
     setTimeout(() => {this.config = {
       title: 'Lista zadań',
       footer: '© Lista zadań, zbudowana w Angularze.',
-      date: new Date()
+      date: new Date(),
     };
     },500);
 
